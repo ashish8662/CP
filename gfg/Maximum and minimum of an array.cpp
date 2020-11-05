5ecaedfe56f6bad1779532cca1977112b46f82d0
Ashish Kumar <ashishkumar8662@gmail.com>
2020-11-05 11:30:53 +0530

    Create Maximum and minimum of an array.cpp

---
+++ gfg/Maximum and minimum of an array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+    int min = arr[0];
+    int max = arr[0];
+    for(int i=1;i<n;i++){
+    if(min > arr[i]) min = arr[i];
+    if(max < arr[i]) max = arr[i];
+    }
+	cout<<"min="<<min<<endl;
+	cout<<"max="<<max<<endl;
+	return 0;
+}
